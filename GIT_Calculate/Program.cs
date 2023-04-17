e6d2e23cdb0ec4a9c3b1a4263ac12d2510abad17
kodev <mr.kodrenko@mail.ru>
2023-04-17 21:26:12 +0300

    feat: create simple calculator

--- GIT_Calculate/Program.cs
+++ GIT_Calculate/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace GIT_Calculate
+﻿using System;
+
+namespace GIT_Calculate
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Первое число.");
+                var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Математическая операция:\n +\n -\n *\n /\n");
+                var operation = Console.ReadLine();
+
+                Console.WriteLine("Второе число.");
+                var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                double result;
+                switch (operation)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Выбрана неверная операция.");
+                        break;
+                }
+            }
         }
     }
 }
